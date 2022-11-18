63d6f6936e3caee595fb137902b44bf675fd3183
PatrickJessen <49081192+PatrickJessen@users.noreply.github.com>
2022-11-18 07:19:01 +0100

    Add project files.

---
+++ H4ImageAPI/Controllers/ImageController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace H4ImageAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ImageController : Controller
+    {
+        ImageToBase64 imageToBase64;
+        [Route("[action]")]
+        [HttpGet]
+        public IActionResult GetImage()
+        {
+            try
+            {
+                imageToBase64 = new ImageToBase64();
+                return Ok(imageToBase64.ToBase64());
+            }
+            catch (Exception e)
+            {
+
+                return Problem(e.Message);
+            }
+        }
+    }
+}
---
+++ H4ImageAPI/ImageToBase64.cs
@@ -0,0 +1,14 @@
+﻿namespace H4ImageAPI
+{
+    public class ImageToBase64
+    {
+        string[] images = { "messi.jpeg", "messi2.jpg", "messi3.png" };
+        public string ToBase64()
+        {
+            Random random = new Random();
+            string image = images[random.Next(images.Length)];
+            byte[] imageArray = File.ReadAllBytes("Images/" + image);
+            return Convert.ToBase64String(imageArray);
+        }
+    }
+}
